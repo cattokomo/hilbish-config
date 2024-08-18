574787816da741f8ab94fc973405798d4b3248e9
cattokomo <71205197+cattokomo@users.noreply.github.com>
2024-08-18 18:40:18 +0800

    add aliases

--- config/aliases.lua
+++ config/aliases.lua
@@ -1,5 +1,9 @@
 local aliases = {
   ["ns"] = "nix-shell --command $SHELL",
+  ["ls"] = "eza --color --icons -F --hyperlink",
+  ["L"] = "ls --git-ignore",
+  ["tree"] = "ls -T",
+  ["T"] = "L -T"
 }
 
 for i, v in pairs(aliases) do
